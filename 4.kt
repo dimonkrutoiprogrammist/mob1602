be6f637cec77429ef171bd537771b02fef1b509c
dimonkrutoiprogrammist <dmitry2008cita@gmail.com>
2026-03-09 18:33:14 +0900

    Update 4.kt

--- 4.kt
+++ 4.kt
@@ -3,5 +3,6 @@ fun printPersonInfo_1(name: String, age: Int = 18, city: String = "Неизве�
 }
 
 fun main(){
-    printPersonInfo_1(city = "Чита", name = "Максим", age = 19)
+    printPersonInfo_1(city = "Чита", name = "Димон", age = 17)
+
 }
